80b0596f3d51a2e24ac4fb1629aa8935ed773378
Joshua Thijssen <jthijssen@noxlogic.nl>
2016-10-04 20:44:40 +0200

    Snakes on 3d planes

--- snake.php
+++ snake.php
@@ -4,7 +4,7 @@ class Snake {
 
     protected $width;
     protected $height;
-    protected $buffer = array();
+    protected $renderBuffers = array();
 
     protected $objects = array();
     protected $snake = array();
@@ -17,31 +17,59 @@ class Snake {
     protected $level = 1;
     protected $numnumnum = 0;
 
+    protected $curPortal = 0;
+
+    const MAX_PLANE = 3;
+
     function __construct($width, $height) {
         $this->width = $width;
         $this->height = $height;
 
         $this->background = array();
-        for ($y = 0; $y != $this->height; $y++) {
-            $this->buffer[$y] = array();
+        for ($z = 0; $z != self::MAX_PLANE; $z++) {
+            $this->renderBuffers[$z] = array();
+            for ($y = 0; $y != $this->height; $y++) {
+                $this->renderBuffers[$z][$y] = array();
+            }
         }
 
         $this->snake = array(
-            $this->getRandomPoint(),
+            $this->getRandomPoint($this->curPortal),
         );
 
         $this->objects = array(
             array('type' => 'food', 'point' => $this->getFreePoint()),
             array('type' => 'bomb', 'point' => $this->getFreePoint()),
-            array('type' => 'hole', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
+            array('type' => 'bomb', 'point' => $this->getFreePoint()),
         );
+
+        $this->generatePortal(0, 1);
+        $this->generatePortal(0, 2);
+        $this->generatePortal(1, 2);
 
         $this->stdin = fopen("php://stdin", "r");
         system("stty -icanon -echo");
     }
 
-    function plotBuffer($x, $y, $fg, $bg, $ch) {
-        $this->buffer[$x][$y] = array($fg, $bg, $ch);
+    function generatePortal($src, $dst) {
+        $h1 = $this->getFreePoint($src);
+        $h2 = $this->getFreePoint($dst);
+
+        $this->objects[] = array('type' => 'portal', 'point' => $h1, 'dst_point' => $h2);
+        $this->objects[] = array('type' => 'portal', 'point' => $h2, 'dst_point' => $h1);
+    }
+
+    function plotBuffer($x, $y, $z, $fg, $bg, $ch) {
+        $this->renderBuffers[$z][$y][$x] = array($fg, $bg, $ch);
     }
 
     function play() {
@@ -54,7 +82,7 @@ class Snake {
     function step()
     {
         $r = array($this->stdin); $w = null; $e = null;
-        stream_select($r, $w, $e, 0, 50 * 1000);
+        stream_select($r, $w, $e, 0, 25 * 1000);
         if ($r) {
             $this->processKeyboard();
         }
@@ -117,7 +145,6 @@ class Snake {
             return;
         }
 
-
         $c = $this->collidesWithObject($new_head, $this->objects);
         if ($c !== false) {
             if ($this->objects[$c]['type'] == 'food') {
@@ -131,6 +158,12 @@ class Snake {
                 array_unshift($this->snake, $new_head);
             } elseif ($this->objects[$c]['type'] == 'bomb') {
                 $this->dead = true;
+            } elseif ($this->objects[$c]['type'] == 'portal') {
+                // Lets move through a portal
+                $new_head = $this->objects[$c]['dst_point'];
+                $this->curPlane = $new_head[2];
+
+                array_unshift($this->snake, $new_head);
             }
         } else {
             array_unshift($this->snake, $new_head);
@@ -141,9 +174,9 @@ class Snake {
         $this->score += $this->level;
     }
 
-    function getFreePoint() {
+    function getFreePoint($plane = null) {
         do {
-            $point = $this->getRandomPoint();
+            $point = $this->getRandomPoint($plane);
         } while ($this->collides($point, $this->snake) !== false);
         
         return $point;
@@ -161,16 +194,28 @@ class Snake {
 
     function collides($point, $points) {
         foreach ($points as $k => $p) {
-            if ($p[0] == $point[0] && $p[1] == $point[1]) {
+            if ($p[0] == $point[0] && $p[1] == $point[1] && $p[2] == $point[2]) {
                 return $k;
             }
         }
         return false;
     }
 
+    function getRandomPoint($plane) {
+        $x = mt_rand(1, $this->width-2);
+        $y = mt_rand(1, $this->height-2);
+        if ($plane === null) {
+            $z = mt_rand(0, self::MAX_PLANE-1);
+        } else {
+            $z = $plane;
+        }
+
+        return array($x, $y, $z);
+    }
+
     function renderSnake() {
         foreach ($this->snake as $point) {
-            $this->plotBuffer($point[0], $point[1], 37, 41, "\xE2\x96\x88");
+            $this->plotBuffer($point[0], $point[1], $point[2], 37, 41, "\xE2\x96\x88");
         }
     }
 
@@ -178,22 +223,24 @@ class Snake {
         foreach ($this->objects as $object) {
             switch ($object['type']) {
                 case 'food' :
-                    $this->plotBuffer($object['point'][0], $object['point'][1], 33, 40, "\xF0\x9F\x8D\xBA");
+                    $this->plotBuffer($object['point'][0], $object['point'][1], $object['point'][2], 33, 40, "\xF0\x9F\x8D\xBA");
                     break;
                 case 'bomb' :
-                    $this->plotBuffer($object['point'][0], $object['point'][1], 33, 40, "\xF0\x9F\x92\xA3");
+                    $this->plotBuffer($object['point'][0], $object['point'][1], $object['point'][2], 33, 40, "\xF0\x9F\x92\xA3");
                     break;
-                case 'hole' :
-                    $this->plotBuffer($object['point'][0], $object['point'][1], 48, 46, "O");
+                case 'portal' :
+                    $this->plotBuffer($object['point'][0], $object['point'][1], $object['point'][2], 48, 46, "O");
                     break;
             }
         }
     }
 
     function renderBackground() {
-        for ($y = 0; $y != $this->height; $y++) {
-            for ($x = 0; $x != $this->width; $x++) {
-                $this->plotBuffer($x, $y, 34, 40, '.');
+        for ($z = 0; $z != self::MAX_PLANE; $z++) {
+            for ($y = 0; $y != $this->height; $y++) {
+                for ($x = 0; $x != $this->width; $x++) {
+                    $this->plotBuffer($x, $y, $z, 34, 40, '.');
+                }
             }
         }
     }
@@ -204,52 +251,78 @@ class Snake {
         $this->renderObjects();
         $this->renderSnake();
 
-        echo "\033[J";
-        echo "\033[H";
+        $s = "";
 
-        echo "\033[36;1m";
-        print "\xE2\x94\x8F".str_repeat("\xE2\x94\x81", $this->width)."\xE2\x94\x93     \n";
+        $s .= "\033[36;1m";
+        for ($plane=0; $plane!=self::MAX_PLANE; $plane++) {
+            $s .= "+". str_repeat("-", $this->width)."+   ";
+        }
+        $s .= "\n";
 
         for ($y = 0; $y != $this->height; $y++) {
-            echo "\033[36;1m";
-            print "\xE2\x94\x83";
 
-            $c_fg = -1;
-            $c_bg = -1;
-            for ($x = 0; $x != $this->width; $x++) {
-                list($fg, $bg, $ch) = $this->buffer[$x][$y];
+            for ($z=0; $z!=self::MAX_PLANE; $z++) {
+                $s .= "\033[36;1m|";
 
-                if ($fg != $c_fg || $bg != $c_bg) {
-                    echo "\033[".$fg."m";
-                    echo "\033[".$bg."m";
-                    $c_fg = $fg;
-                    $c_bg = $bg;
+                $c_fg = -1;
+                $c_bg = -1;
+                for ($x = 0; $x != $this->width; $x++) {
+                    list($fg, $bg, $ch) = $this->renderBuffers[$z][$y][$x];
+
+                    if ($fg != $c_fg || $bg != $c_bg) {
+                        $s .= "\033[" . $fg . "m";
+                        $s .= "\033[" . $bg . "m";
+                        $c_fg = $fg;
+                        $c_bg = $bg;
+                    }
+                    $s .= $ch;
                 }
-                echo $ch;
-            }
 
-            echo "\033[36;1m";
-            print "\xE2\x94\x83";
-            echo "   \n";
+                $s .= "\033[36;40;1m|   ";
+            }
+            $s .= "\n";
         }
 
-        echo "\033[36;1m";
-        print "\xE2\x94\x97".str_repeat("\xE2\x94\x81", $this->width)."\xE2\x94\x9B\n";
+        $s .= "\033[36;1m";
+        for ($plane=0; $plane!=self::MAX_PLANE; $plane++) {
+            $s .= "+". str_repeat("-", $this->width)."+   ";
+        }
+        $s .= "\n";
 
-        echo "\033[37;1m";
-        print "                            <<< SNAKES ON A PLAIN >>>\n";
-        printf("                   Level %02d   Score: %06d    Food: %03d", $this->level, $this->score, $this->numnumnum);
+        $s .= "\033[37;1m";
+        $s .= "                                                                                      ";
+        $s .= sprintf("                   Level %02d   Score: %06d    Food: %03d", $this->level, $this->score, $this->numnumnum);
 
-        echo "\033[39;49m";
-        echo "\033[0m";
-    }
+        $s .= "\n\n\n";
 
-    function getRandomPoint() {
+        $s .= "\033[35;1m";
 
-        $x = mt_rand(1, $this->width-2);
-        $y = mt_rand(1, $this->height-2);
 
-        return array($x, $y);
+$airplane = <<< SAMUELLJACKSON
+                      _________________________          _____
+                     |                         \          \ U \__      _____
+                     |   Snake(s) on a plane    \__________\   \/_______\___\_____________
+                     |   BGPHP16 Hackathon      /          < /_/   .....................  `-.
+                     |_________________________/            `-----------,----,--------------'
+                                                                      _/____/
+SAMUELLJACKSON;
+
+//        $this->ctr++;
+//        foreach (explode("\n", $airplane) as $line) {
+//            $s .= str_repeat(" ", $this->ctr) . $line . "\n";
+//        }
+        $s .= $airplane;
+
+        $s .= "\n\n\n";
+
+        $s .= "\033[39;49m";
+        $s .= "\033[0m";
+
+
+
+        print "\033[J";
+        print "\033[H";
+        print $s;
     }
 
 }
